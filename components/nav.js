4fd9d633c223c429f864c47a1eaa151b5470a789
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-11-04 12:20:09 -0800

    adjusted links, styling of nav

--- components/nav.js
+++ components/nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-const links = [{ href: '/shop', label: 'Shop' }, { href: '/faq', label: 'FAQ' }].map(link => {
+const links = [{ href: '/faq', label: 'FAQ' }].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
 });
@@ -11,7 +11,9 @@ const Nav = () => (
     <ul>
       <li>
         <Link href="/">
-          <a className="brand">Dad hats with text</a>
+          <a className="brand">
+            <img className="logo" src="/static/logo.png" />
+          </a>
         </Link>
       </li>
       {links.map(({ key, href, label }) => (
@@ -30,13 +32,15 @@ const Nav = () => (
         text-align: center;
         display: grid;
         grid-template-columns: repeat(12, 1fr);
+        padding: 0 16px;
       }
       ul {
         display: flex;
         justify-content: flex-start;
+        align-items: center;
       }
       nav > ul {
-        padding: 4px 16px;
+        padding: 0;
         grid-column: 1 / span 12;
       }
       li {
@@ -49,7 +53,11 @@ const Nav = () => (
         font-size: 1em;
       }
       a.brand {
-        font-weight: bold;
+        display: inline-block;
+      }
+      img.logo {
+        max-width: 64px;
+        height: auto;
       }
       a:hover {
         text-decoration: underline;
